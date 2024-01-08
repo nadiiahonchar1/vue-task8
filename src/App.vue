33ddb4f086576c9bd8e34c67e4ec61bdc508613b
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-01-08 16:48:02 +0200

    added AppAlert

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div></div>
+  <div class="container">
+    <app-alert
+      v-if="alert"
+      text="This is warning"
+      title="Warning!"
+      type="danger"
+      @close="alert = false"
+      closable
+    ></app-alert>
+    <div class="cadr">
+      <button class="btn primary" @click="toggleAlert">
+        {{ alert ? "Hide" : "Show" }} alert
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import AppAlert from "./components/AppAlert.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { AppAlert },
+  data() {
+    return {
+      alert: false,
+    };
+  },
+  methods: {
+    toggleAlert() {
+      this.alert = !this.alert;
+    },
+  },
 };
 </script>
 
@@ -17,5 +43,67 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color: rgb(255 255 0 / 30%);
+}
+
+.container {
+  margin: 0 auto;
+  width: 50vw;
+}
+
+.card {
+  border: 1px solid rgb(87, 87, 199);
+  border-radius: 10px;
+}
+
+.card-title {
+  margin-bottom: 10px;
+}
+
+.card {
+  margin-bottom: 5px;
+  padding: 5px 20px;
+}
+.centeder {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+.btn {
+  display: inline-block;
+  min-width: 100px;
+  padding: 5px;
+  border-radius: 20px;
+  background-color: transparent;
+  border: 1px solid #2756a8ad;
+  color: #2756a8ad;
+  cursor: pointer;
+}
+
+.danger {
+  color: rgb(219, 29, 29);
+  border: 1px solid red;
+}
+
+.primary,
+.btn:hover {
+  border: 1px solid #2756a8ad;
+  background-color: #2756a8ad;
+  color: white;
+}
+
+.btn:hover {
+  border-color: #022950;
+}
+
+[disabled] {
+  background-color: #ccc7ba;
+}
+
+.danger:hover {
+  color: rgb(255 151 0);
+  border: 1px solid rgb(255 151 0);
 }
 </style>
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="alert" :class="type">
+    <h3 class="alert-title">{{ title }}</h3>
+    <p class="alert-text">{{ text }}</p>
+    <button class="btn" :class="type" v-if="closable" @click="$emit('close')">
+      Close
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+  props: {
+    text: String,
+    title: String,
+    closable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "primary",
+      validator(val) {
+        return ["primary", "danger", "warning"].includes(val);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.alert {
+  border: 2px double white;
+  border-radius: 20px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+.primary {
+  background: linear-gradient(to right, blue, 5%, yellow, 75%, white);
+  color: blue;
+}
+.danger {
+  background: linear-gradient(to right, red, 5%, yellow, 75%, white);
+  color: red;
+}
+.warning {
+  background: linear-gradient(to right, orange, 5%, yellow, 75%, white);
+  color: orange;
+}
+</style>
